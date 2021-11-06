35ebb70d858fbbdafd54d240a904014a895e552d
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-11-06 21:49:35 +0545

    install and configure multer

---
+++ middlewares/multer.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+// multer file filter
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/webp"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
